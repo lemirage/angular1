8187d7af54a10dedf0f1536c697d4e1d43078af5
william <william91120@hotmail.com>
2016-06-13 12:24:39 +0200

    fin matiner 2 way data binding

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ angular.module('testApp',[])
   .controller('mainCtrl',function($scope){
 
     $scope.title = 'Formation Angular';
-    $scope.title = 'blabla';
+    //  $scope.title = 'blabla';
 
     $scope.clients = ['client1','client2', 'client3'];
 
@@ -10,24 +10,55 @@ angular.module('testApp',[])
       firstname: 'Paul',
       lastname: 'pogba',
       number: 15,
-      selected:true
+      selected:true,
+      img:"pogba.jpg"
     };
 
     var joueur2 = {
       firstname: 'Hugo',
       lastname: 'lloris',
       number: 1,
-      selected:true
+      selected:true,
+      img:"lloris.jpg"
     };
 
     var joueur3 = {
       firstname: 'toto',
       lastname: 'titi',
       number: 17,
-      selected:false
+      selected:false,
+      img:""
     };
 
-
+    $scope.playerNumber = 89;
     $scope.joueurs = [joueur1,joueur2,joueur3];
 
+    $scope.majus = function(){
+      console.log("click sur le boutton");
+      console.log($scope.joueurs);
+      for (var i = 0; i < $scope.joueurs.length; i++) {
+        $scope.joueurs[i].firstname = $scope.joueurs[i].firstname.toUpperCase();
+      }
+    };
+
+    $scope.displayImg = function(joueur){
+      console.log("in");
+
+      console.log( $scope.imgjoueur);
+      $scope.playerNumber = joueur.number;
+    //  $scope.imgjoueur.src=img;
+      //$scope.imgjoueur.display = "block";
+
+
+
+    };
+    $scope.hideImg = function(joueur){
+      console.log("out");
+      $scope.playerNumber = "joueur.number";
+    //  $scope.imgjoueur.display = "none";
+    };
+
+  }) // fin de premier controller
+  .controller('playerCtrl',function($scope){
+    $scope.title = "Je suis là";
   });
